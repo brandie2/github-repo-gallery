09444cab41862275e422f9ed896532f93187fd66
B <Brandie.costello@gmail.com>
2023-01-30 11:04:24 -0800

    added functions to pull and display repo info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //where profile information will appear
 const overview = document.querySelector(".overview");
 const username = "brandie2";
+const repoList = document.querySelector(".repo-list");
 
 
 //async function to fetch info from profile
@@ -29,4 +30,25 @@ const displayUserInfo = function (data) {
     </div>
     `;
     overview.append(div);
+    gitRepos();
+};
+
+
+//async function to fetch repos
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    //console.log(fetchRepos());
+    displayRepos(repoData);
+};
+
+//function to display info about each repo
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement('li');
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+
 };
